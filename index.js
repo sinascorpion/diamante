f2c792ad73ff44ce07c5685d4b9e909c19ee3158
Sina <s.scorpion2020@gmail.com>
2025-12-14 04:54:21 -0600

    Update creator name in goodbye message

--- index.js
+++ index.js
@@ -1144,7 +1144,7 @@ async function main() {
         console.log(`\n${colors.bold}${colors.cyan}============================================`);
         console.log(`       Goodbye! Thanks for using`);
         console.log(`============================================`);
-        console.log(`${colors.bold}${colors.magenta}       Created by Kazuha`);
+        console.log(`${colors.bold}${colors.magenta}       Created by sinascorpion`);
         console.log(`       github.com/sinascorpion${colors.reset}`);
         console.log(`${colors.bold}${colors.cyan}============================================${colors.reset}\n`);
         process.exit(0);
